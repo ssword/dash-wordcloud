3f237708010c13595a16fced2b68dac03654fd1f
Gang Chen <ssword@gmail.com>
2018-04-17 19:27:04 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+exec (open('dash_wordcloud/version.py').read())
+
+setup(
+    name='dash_wordcloud',
+    version=__version__,
+    author='ssword',
+    packages=['dash_wordcloud'],
+    include_package_data=True,
+    license='MIT',
+    description='A Dash UI component for wordcloud visualization',
+    install_requires=[]
+)
