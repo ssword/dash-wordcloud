4437193f1240a1cb8caf836f33e0c477010d8fa3
Gang Chen <ssword@gmail.com>
2018-05-02 12:50:33 -0500

    reajustified parameter

--- src/components/Wordcloud.react.js
+++ src/components/Wordcloud.react.js
@@ -6,16 +6,14 @@ import WordCloud from 'wordcloud';
 export default class Wordcloud extends Component {
     constructor(props) {
         super(props);
-        this.width = 1250;
-        this.height = 700;
     }
 
     componentDidMount() {
         var el = this.refs['my-canvas']
 
         var newCanvas = document.createElement('canvas');
-        newCanvas.height = this.height;
-        newCanvas.width = this.width;
+        newCanvas.height = this.props.height;
+        newCanvas.width = this.props.width;
         newCanvas.id = 'canvas';
         el.appendChild(newCanvas);
         this.newLabel(el);
@@ -43,119 +41,162 @@ export default class Wordcloud extends Component {
 
 
 Wordcloud.propTypes = {
-    // TODO: reconsider the PropTypes for each parameter.
     /**
-     * The ID used to identify this compnent in Dash callbacks
+     * Width of the canvas
+     * Default: 1024
      */
-    id: PropTypes.string,
+    width: PropTypes.number,
+    /**
+     * Height of the canvas
+     * Default: 960
+     */
+    height: PropTypes.number,
     /**
      * List of words/text to paint on the canvas in a 2-d array, 
      * in the form of [word, size], e.g. [['foo', 12], ['bar', 6]].
+     * Default: Les Misérables
      */
-    list: PropTypes.any,
+    list: PropTypes.arrayOf(PropTypes.array),
     /**
-     * Font to use.
+     * Font to use. 
+     * Default:
+     * 'Segoe UI',
      */
     fontFamily: PropTypes.string,
     /**
-     * font weight to use, can be, as an example, normal, bold or 600 or 
-     * a callback(word, weight, fontSize specifies different font-weight 
+     * Font weight to use, can be, as an example, normal, bold or 600 or 
+     * a callback(word, weight, fontSize) specifies different font-weight 
      * for each item in the list.
+     * Default: 'bold'
      */
-    fontWeight: PropTypes.string,
+    fontWeight: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.number,
+        PropTypes.func
+    ]),
     /**
-     * color of the text, can be any CSS color, or
-     *  a callback(word, weight, fontSize, distance, theta) 
+     * Color of the text, can be any CSS color, or
+     * a callback(word, weight, fontSize, distance, theta) 
      * specifies different color for each item in the list. 
      * You may also specify colors with built-in keywords: 
      * random-dark and random-light. If this is a DOM cloud,
-     *  color can also be null to disable hardcoding of 
+     * color can also be null to disable hardcoding of 
      * color into span elements 
      * (allowing you to customize at the class level).
+     * Default: 'random-dark'
      */
-    color: PropTypes.string,
+    color: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.arrayOf(PropTypes.string),
+        PropTypes.func,
+        PropTypes.oneOf(['random-dark', 'random-light'])
+    ]),
     /**
-     * minimum font size to draw on the canvas.
+     * Minimum font size to draw on the canvas.
+     * 0 to disable
+     * Default: 0
      */
     minSize: PropTypes.number,
     /**
-     *function to call or number to multiply for size of each word in the list.
+     * Function to call or number to multiply for 
+     * size of each word in the list.
+     * Default: 1
      */
-    weightFactor: PropTypes.number,
+    weightFactor: PropTypes.oneOfType([
+        PropTypes.number,
+        PropTypes.func
+    ]),
     /**
-     * paint the entire canvas with background color and 
+     * Paint the entire canvas with background color and 
      * consider it empty before start.
+     * Default: true
      */
     clearCanvas: PropTypes.bool,
     /**
-     * color of the background.
+     * Color of the background.
+     * Default: 'white'
      */
     backgroundColor: PropTypes.string,
     /**
-     * size of the grid in pixels for marking the availability of the canvas —
-     *  the larger the grid size, the bigger the gap between words.
+     * Size of the grid in pixels for marking the availability of the canvas —
+     * the larger the grid size, the bigger the gap between words.
+     * Default: 0
      */
     gridSize: PropTypes.number,
     /**
-     * set to true to allow word being draw partly outside of the canvas. 
+     * Set to true to allow word being draw partly outside of the canvas. 
      * Allow word bigger than the size of the canvas to be drawn.
+     * Default: false
      */
     drawOutOfBound: PropTypes.bool,
     /**
-     *  origin of the "cloud" in [x, y].
+     * Origin of the "cloud" in [x, y].
+     * Default: null
      */
-    origin: PropTypes.any,
+    origin: PropTypes.arrayOf(PropTypes.number),
     /**
-     * visualize the grid by draw squares to mask the drawn areas.
+     * Visualize the grid by draw squares to mask the drawn areas.
+     * Default: false
      */
     drawMask: PropTypes.bool,
     /**
-     * color of the mask squares.
+     * Color of the mask squares.
+     * Default: 'rgba(255,0,0,0.3)'
      */
     maskColor: PropTypes.string,
     /**
-     * width of the gaps between mask squares.
+     * Width of the gaps between mask squares.
+     * Default: 0.3
      */
     maskGapWidth: PropTypes.number,
     /**
      * Wait for x milliseconds before start drawn the next item 
      * using setTimeout.
+     * Default: 0
      */
     wait: PropTypes.number,
     /**
      *  If the call with in the loop takes more than x milliseconds 
      * (and blocks the browser), abort immediately.
+     * 0 to disable
+     * Default: 0
      */
     abortThreshold: PropTypes.number,
     /**
-     * callback function to call when abort.
+     * Callback function to call when abort.
+     * Default: function noop() {}
      */
     abort: PropTypes.func,
     /**
      * If the word should rotate, 
      * the minimum rotation (in rad) the text should rotate.
+     * Default: -Math.PI / 4
      */
     minRotation: PropTypes.number,
     /**
      *  If the word should rotate, 
      * the maximum rotation (in rad) the text should rotate. 
      * Set the two value equal to keep all text in one angle.
+     * Default: Math.PI / 4
      */
     maxRotation: PropTypes.number,
     /**
      * Force the use of a defined number of angles. 
      * Set the value equal to 2 in a -90 degree/90 degree range means
      *  just -90, 0 or 90 will be used.
+     * Default: 0
      */
     rotationSteps: PropTypes.number,
     /**
      * Shuffle the points to draw so the result will be 
      * different each time for the same list and settings.
+     * Default: true
      */
     shuffle: PropTypes.bool,
     /**
      * Probability for the word to rotate. 
      * Set the number to 1 to always rotate.
+     * Default: 0.4
      */
     rotateRatio: PropTypes.number,
     /**
@@ -167,36 +208,49 @@ Wordcloud.propTypes = {
      * the most known polar equation), diamond, square,
      *  triangle-forward, triangle, 
      * (alias of triangle-upright, pentagon, and star.
-     */
-    shape: PropTypes.string,
+     * Default: 'circle'
+     */ 
+    shape: PropTypes.oneOf([
+        'circle', 'cardioid', 'diamond', 'square', 'triangle-forward', 
+        'triangle', 'triangle-upright', 'pentagon', 'star'
+    ]),
     /**
-     * degree of "flatness" of the shape wordcloud2.js should draw.
+     * Degree of "flatness" of the shape wordcloud2.js should draw.
+     * Default: 0.65
      */
     ellipticity: PropTypes.number,
     /**
-     *for DOM clouds, allows the user to define the class of the span elements.
+     * For DOM clouds, allows the user to define the class of the span elements.
      * Can be a normal class string, applying the same class to every span or
      * a callback(word, weight, fontSize, distance, theta) for per-span class
-     *  definition. 
+     * definition. 
      * In canvas clouds or if equals null, this option has no effect.
+     * Default: null
      */
-    classes: PropTypes.any,
+    classes: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.func
+    ]),
     /**
-     * callback to call when the cursor enters or 
+     * Callback to call when the cursor enters or 
      * leaves a region occupied by a word.
-     *  The callback will take arguments callback(item, dimension, event),
-     *  where event is the original mousemove event.
+     * The callback will take arguments callback(item, dimension, event),
+     * where event is the original mousemove event.
+     * Default: hover function with text frame and word frequency tooltip
      */
     hover: PropTypes.func,
     /**
-     * callback to call when the user clicks on a word.
-     *  The callback will take arguments callback(item, dimension, event),
-     *  where event is the original click event.
+     * Callback to call when the user clicks on a word.
+     * The callback will take arguments callback(item, dimension, event),
+     * where event is the original click event.
+     * Default: null
      */
     click: PropTypes.func
 };
 
 Wordcloud.defaultProps = {
+    width: 1024,
+    height: 960,
     list: [
         ['Les Misérables', 30], ['Victor Hugo', 20], ['Jean Valjean', 15],
         ['Javert', 15], ['Fantine', 15], ['Cosette', 15],
