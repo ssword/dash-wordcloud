b6ce1432fcb64c139758e10eda06788df2058065
Gang Chen <ssword@gmail.com>
2018-05-15 03:46:18 -0500

    add README into pypi Description.

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 exec(open('dash_wordcloud/version.py').read())
 
@@ -11,5 +16,7 @@ setup(
     include_package_data=True,
     license='MIT',
     description='A Dash UI component for wordcloud visualization',
-    install_requires=[]
+    install_requires=[],
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
